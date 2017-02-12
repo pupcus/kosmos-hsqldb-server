24c5874c5b1b0d22e5e2cc88194641712c9d853b
Michael Pendergrass <mdpendergrass@gmail.com>
2017-02-12 09:12:45 -0500

    update dependencies

--- project.clj
+++ project.clj
@@ -9,13 +9,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[kosmos "0.0.3"]
+  :dependencies [[kosmos "0.0.6"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [org.hsqldb/hsqldb "2.3.3"]]
+                 [org.hsqldb/hsqldb "2.3.4"]]
 
   :profiles {:dev {:resource-paths ["dev-resources"]
                    :dependencies   [[org.clojure/clojure "1.8.0"]
-                                    [org.clojure/java.jdbc "0.4.2"]
+                                    [org.clojure/java.jdbc "0.6.1"]
                                     [org.slf4j/slf4j-log4j12 "1.7.5"]]}}
 
   :deploy-repositories [["snapshots"
